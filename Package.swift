0cdea50ab90d9bd63645d2d51bcef5850ca4aa9f
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2024-04-08 13:20:46 +0300

    Dynamic dependency path update v6.14.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.14.0/AppsFlyerLib-Dynamic.xcframework.zip",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.14.0/AppsFlyerLib-Dynamic-SPM.xcframework.zip",
             checksum: "abc3d9ffd21554e143211944b7989e3c3481870130be985d8f554b13b957e93f"
         )
     ]
